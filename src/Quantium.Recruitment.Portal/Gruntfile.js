8d97fff5c45cb9695b8e559c75a371e3fc7e9b43
Rakesh Rohan A <rkshrohan@gmail.com>
2016-08-14 11:26:09 +0530

    feat: added responsiveness

--- src/Quantium.Recruitment.Portal/Gruntfile.js
+++ src/Quantium.Recruitment.Portal/Gruntfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding ProjectOpened='watch:less' />
+/// <binding ProjectOpened='watch:less, less:dist' />
 // This file in the main entry point for defining grunt tasks and using grunt plugins.
 // Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 
