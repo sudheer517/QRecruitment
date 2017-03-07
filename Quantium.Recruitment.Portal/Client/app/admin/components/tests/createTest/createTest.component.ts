7a39837a43a1959eb02791b837aa8589fbf1b55d
Rakesh Rohan A <rkshrohan@gmail.com>
2017-03-07 19:28:27 +0530

    feat: added create test initial

--- Quantium.Recruitment.Portal/Client/app/admin/components/tests/createTest/createTest.component.ts
+++ Quantium.Recruitment.Portal/Client/app/admin/components/tests/createTest/createTest.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JobService } from '../../../services/job.service';
+import { JobDto } from '../../../../RemoteServicesProxy';
 
 @Component({
     selector: 'appc-create-test',
     templateUrl: './createTest.component.html'
 })
-export class CreateTestComponent{
+export class CreateTestComponent implements OnInit{
+    jobs: JobDto[];
+    selectedJobId: number;
+    selectedJob: JobDto;
 
+    selectJob(jobId: number): void { 
+        this.selectedJob = this.jobs.find(j => j.Id == jobId);
+    }
+
+    constructor(private jobService: JobService){
+
+    }
+
+    ngOnInit(){
+        this.jobService.GetAllJobs().subscribe(
+            jobs => this.jobs = jobs,
+            error => console.log(error)
+        )
+    }
+
+    generateTests(){
+        console.log("test created");
+    }
 }
