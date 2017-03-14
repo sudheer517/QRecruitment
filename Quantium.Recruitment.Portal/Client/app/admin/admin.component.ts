d608791e50b55543395a273ac8a9b6b46003992b
Rakesh Rohan A <rkshrohan@gmail.com>
2017-03-15 01:50:23 +0530

    fix: tab layout sidenav & animation fixed

--- Quantium.Recruitment.Portal/Client/app/admin/admin.component.ts
+++ Quantium.Recruitment.Portal/Client/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 // import { routerTransition, hostStyle } from '../router.animations';
 //import {NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap';
 
@@ -6,12 +6,36 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'appc-admin',
   styleUrls: ['./admin.component.scss'],
   templateUrl: './admin.component.html',
-  // animations: [routerTransition()],
+  animations: [
+      trigger("visibility",[
+          state("shown", style({
+            left: '0px'
+          })),
+          state("hidden", style({
+            left: '-200px',
+          })),
+          transition("* <=> *", animate("200ms"))
+          // transition("* <=> *", [
+          //   style({opacity: '0'}),
+          //   animate("500ms")
+          // ])
+      ])
+  ],
   // encapsulation: ViewEncapsulation.Native,
   // tslint:disable-next-line:use-host-property-decorator
   // host: hostStyle()
 })
 export class AdminComponent {
+  visibility: string = "hidden";
+  setDisplayNoneForSideNav = true;
+  isNavbarCollapsed: boolean;
+
   constructor() {
+  }
+
+  toggleNavButton(){
+    this.setDisplayNoneForSideNav = false;
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+    this.visibility = (this.visibility === "hidden" ? "shown" : "hidden");
   }
  }
