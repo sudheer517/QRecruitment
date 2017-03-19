0f369a89511d46bc950afca47833305b90ba5734
Rakesh Rohan A <rkshrohan@gmail.com>
2017-03-20 04:54:12 +0530

    fix: candidate page routing fixed

--- Quantium.Recruitment.Portal/Client/app/candidate/services/instructions.guard.ts
+++ Quantium.Recruitment.Portal/Client/app/candidate/services/instructions.guard.ts
@@ -8,18 +8,17 @@ export class InstructionsGuard implements CanActivate {
   constructor(private router: Router, private candidateService: CandidateService, private activatedRoute: ActivatedRoute) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
-    return true;
-    // return this.candidateService.IsInformationFilled()._do(
-    //   isFilled => {
-    //     if(isFilled === true){
-    //       console.log("navigatiing to instruc");
-    //       return true;
-    //     }
-    //     else{
-    //       console.log("navigatiing to cand details");
-    //       this.router.navigate(["/candidate/candidateDetails"]);
-    //       return false;
-    //     }
-    //   });
+    return this.candidateService.IsInformationFilled().do(
+      isFilled => {
+        if(isFilled === true){
+          console.log("navigatiing to instruc");
+          return true;
+        }
+        else{
+          console.log("navigatiing to cand details");
+          this.router.navigate(["/candidate/candidateDetails"]);
+          return false;
+        }
+      });
   }
 }
