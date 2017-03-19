473f5e8f1982697b8caa7b71509c4eb2755f0079
Rakesh Rohan A <rkshrohan@gmail.com>
2017-03-20 03:20:37 +0530

    fix: commented guard in candidate module to make it work

--- Quantium.Recruitment.Portal/Client/app/candidate/services/test.guard.ts
+++ Quantium.Recruitment.Portal/Client/app/candidate/services/test.guard.ts
@@ -8,19 +8,19 @@ export class TestGuard implements CanActivate {
   constructor(private router: Router, private challengeService: ChallengeService, private activatedRoute: ActivatedRoute) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
-    
-    return this.challengeService.IsTestFinished().do(
-      isFinished => {
-        if(isFinished === true){
-          console.log("isfinshed is true");
-          this.router.navigate(["/candidate/testFinished"]);
-          return false;
-        }
-        else{
-          console.log("isfinshed is false");
-          this.router.navigate(["/candidate/instructions"]);
-          return true;
-        }
-      });
+    return true;
+    // return this.challengeService.IsTestFinished().do(
+    //   isFinished => {
+    //     if(isFinished === true){
+    //       console.log("isfinshed is true");
+    //       this.router.navigate(["/candidate/testFinished"]);
+    //       return false;
+    //     }
+    //     else{
+    //       console.log("isfinshed is false");
+    //       this.router.navigate(["/candidate/instructions"]);
+    //       return true;
+    //     }
+    //   });
   }
 }
