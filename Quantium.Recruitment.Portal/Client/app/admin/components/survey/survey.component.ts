5a5d779e8d60fc061b37f89545bbd2ce9d90ce1f
bhanu499 <bhanu499@gmail.com>
2017-09-01 16:39:52 +0530

    change made to redirect by default to child route in survey admin page

--- Quantium.Recruitment.Portal/Client/app/admin/components/survey/survey.component.ts
+++ Quantium.Recruitment.Portal/Client/app/admin/components/survey/survey.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation, ElementRef, Renderer } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'appc-survey',
@@ -10,11 +11,12 @@ export class SurveyComponent implements OnInit {
     locationChartOptions: Object;
     branchChartOptions: Object;
     teamChartOptions: Object;
-    constructor(private el: ElementRef, private renderer: Renderer) {
+    constructor(private el: ElementRef, private renderer: Renderer, private router: Router, private activatedRoute: ActivatedRoute) {
     }
 
     ngOnInit() {
-       this.changeBackground();
+        // this.changeBackground();
+        this.router.navigate(["surveyQuestions"], { relativeTo: this.activatedRoute });        
     }
 
     changeBackground() {
